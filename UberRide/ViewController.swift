51e0a013a1912491071de95bc0a3ebc5d632b2de
Efrain <jesusayllonroa@gmail.com>
2016-08-05 23:40:43 -0500

    Done!

--- UberRide/ViewController.swift
+++ UberRide/ViewController.swift
@@ -9,17 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @NSCopying var timeZone: NSTimeZone?
+    
+    var notificationDate :String!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var myDate: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        datePicker.hidden = false
+        myDate.hidden = true
 
+        super.viewDidLoad()
+        datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)    }
+    
+    func datePickerValueChanged (datePicker: UIDatePicker) {
+        
+        let dateformatter = NSDateFormatter()
+        
+        dateformatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateformatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        let outputDate = dateformatter.stringFromDate(datePicker.date)
+
+        myDate.text = outputDate
+        notificationDate = outputDate
+    }
+    
+    
+    
+    
+    @IBAction func readyButtonisPressed() {
+        
+        datePicker.hidden = false
+        myDate.hidden = true
+        
+        
+    }
+    
+    @IBAction func readyButtonCompleted() {
+        let notifaction = UILocalNotification()
+        notifaction.alertTitle = "Your ride is here!"
+        notifaction.alertBody = "Ride is here. Are you ready?"
+        notifaction.alertAction = "Hello!"
+        notifaction.fireDate = datePicker.date
+        notifaction.category = "actionCategory"
+        notifaction.userInfo = ["alarmDate":datePicker.date]
+        UIApplication.sharedApplication().scheduleLocalNotification(notifaction)
+        [AZNotification .showNotificationWithTitle("Congrats! Your Uber is on their way", controller: self, notificationType: AZNotificationType.Success)]
+        [AZNotificationType.Success]
+    
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
